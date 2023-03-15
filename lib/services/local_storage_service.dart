4ddca506a99b80e2ed288ad139bc26a87c84c7b0
Viacheslav Romanov <viacheslav.romanov@autodesk.com>
2023-03-15 10:05:41 +0900

    Main Add web support

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -1,10 +1,10 @@
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
 class LocalStorageService {
   static Future<void> initialize() async {
-    final directory = await getApplicationDocumentsDirectory();
-    Hive.init(directory.path);
+    Hive.initFlutter();
   }
 
   Future<String?> get(String key) async {
